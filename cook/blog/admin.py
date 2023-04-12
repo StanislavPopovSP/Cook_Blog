232d3255c1809cf7c4563b61cb4ebd05e529f8d7
StanislavPopovSP <staspopov4@gmail.com>
2023-04-12 20:20:16 +0500

    Зарегистрировал модели, привязал slug к имени

--- cook/blog/admin.py
+++ cook/blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Post)
+admin.site.register(Recipe)
+admin.site.register(Comment)
