1e21e89370dd78491968eab0833eecdc3ea20632
StanislavPopovSP <staspopov4@gmail.com>
2023-04-12 20:18:31 +0500

    Перевёл Админку на русский язык и свзязал своё время с БД.

--- cook/config/settings.py
+++ cook/config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'mptt', # подключил mptt библиотеку
+
     'blog.apps.BlogConfig',
 ]
 
@@ -104,13 +107,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-Ru'
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
