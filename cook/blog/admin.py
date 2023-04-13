a454d2b79b8ff8d1c38cccd60227945d970b5a21
Станислав <staspopov4@gmail.com>
2023-04-13 12:11:59 +0500

    Добавил вывод нужных полей в админку

--- cook/blog/admin.py
+++ cook/blog/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from mptt.admin import MPTTModelAdmin
-
 from .models import *
 
 
@@ -11,13 +10,18 @@ class CustomMPTTModelAdmin(MPTTModelAdmin):
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
 
-
+@admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
 
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'author', 'create_at', 'id']
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'prep_time', 'cook_time', 'post']
+
 
 admin.site.register(Category, CustomMPTTModelAdmin)
-admin.site.register(Tag, TagAdmin)
-admin.site.register(Post)
-admin.site.register(Recipe)
 admin.site.register(Comment)
