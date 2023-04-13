74b1513f80e896fa0080460109c4feb7dfec1447
Станислав <staspopov4@gmail.com>
2023-04-13 14:05:17 +0500

    Добавил соединение таблицы Post с Recipe

--- cook/blog/admin.py
+++ cook/blog/admin.py
@@ -4,22 +4,36 @@ from .models import *
 
 
 class CustomMPTTModelAdmin(MPTTModelAdmin):
+    """Количество пикселей, для вложенных категорий"""
     mptt_level_indent = 20
 
 
-class CategoryAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('name',)}
+# class CategoryAdmin(admin.ModelAdmin):
+#     prepopulated_fields = {'slug': ('name',)}
+
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
+    """Регистрация и вывод нужных полей"""
     prepopulated_fields = {'slug': ('name',)}
+
+
+class RecipeInline(admin.StackedInline):
+    """Для соединения Рецептов с Постом"""
+    model = Recipe
+    extra = 1 # для вывода одного рецепта
+
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
+    """Регистрация и вывод нужных полей"""
     list_display = ['title', 'category', 'author', 'create_at', 'id']
+    inlines = [RecipeInline] # подключение модели Recipe
+
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
+    """Регистрация и вывод нужных полей"""
     list_display = ['name', 'prep_time', 'cook_time', 'post']
 
 
