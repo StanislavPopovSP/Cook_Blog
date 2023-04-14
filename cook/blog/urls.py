9dcce91fac89cf832080bef3c43b397706362461
StanislavPopovSP <staspopov4@gmail.com>
2023-04-15 00:01:49 +0500

    Добавил путь для просмотра отдельной статьи

--- cook/blog/urls.py
+++ cook/blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'), # путь ведущий на главную страницу
-    path('<slug:slug>/', views.PostListView.as_view(), name='post_list') # путь для какой-то категории
+    path('<slug:slug>/', views.PostListView.as_view(), name='post_list'), # путь для какой-то категории
+    path('<slug:slug>/<slug:post_slug>/', views.PostDetailView.as_view(), name='post_single'), # путь для просмотра отдельной статьи
 ]
