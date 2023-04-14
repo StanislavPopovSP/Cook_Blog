0df7be9882422918636b0f2eb028986d3f3a28ac
StanislavPopovSP <staspopov4@gmail.com>
2023-04-14 15:44:47 +0500

    Добавил обработчик для вывода постов из определенной категории.

--- cook/blog/views.py
+++ cook/blog/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import *
+
+
+class PostListView(ListView):
+    """Категория определенной статьи"""
+    model = Post
+
+    def get_queryset(self) -> list:
+        """Фильтруем посты по нужной категории, переопределяем доступ к БД."""
+        return Post.objects.filter(category__slug=self.kwargs['slug']).select_related('category')
 
 def home(request):
     return render(request, 'base.html')
