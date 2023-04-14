e3a3b2de358902af817bfeab6c118c9eab3a67ef
StanislavPopovSP <staspopov4@gmail.com>
2023-04-15 00:10:39 +0500

    Добавил обработчик PostDetailView для просмотра поста

--- cook/blog/views.py
+++ cook/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+
 from .models import *
 
 
@@ -10,6 +11,14 @@ class PostListView(ListView):
     def get_queryset(self) -> list:
         """Фильтруем посты по нужной категории, переопределяем доступ к БД."""
         return Post.objects.filter(category__slug=self.kwargs['slug']).select_related('category')
+
+
+class PostDetailView(DetailView):
+    """Просмотр отдельного поста"""
+    model = Post
+    slug_url_kwarg = 'post_slug'
+    context_object_name = 'post'
+
 
 def home(request):
     return render(request, 'base.html')
