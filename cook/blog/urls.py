312dfd5d050092f2437158fb98280f4ec658786b
StanislavPopovSP <staspopov4@gmail.com>
2023-04-14 15:40:36 +0500

    Добавил путь для какой-то категории

--- cook/blog/urls.py
+++ cook/blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home') # путь ведущий на главную страницу
+    path('', views.home, name='home'), # путь ведущий на главную страницу
+    path('<slug:slug>/', views.PostListView.as_view(), name='post_list') # путь для какой-то категории
 ]
