7ef9e9c02325f17966c58a25dfc3e25fb5a4f05e
StanislavPopovSP <staspopov4@gmail.com>
2023-04-15 20:51:42 +0500

    Изменил обработчик ведущий на главную страницу.

--- cook/blog/urls.py
+++ cook/blog/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'), # путь ведущий на главную страницу
+    path('', views.HomeView.as_view(), name='home'), # путь ведущий на главную страницу
+
     path('<slug:slug>/', views.PostListView.as_view(), name='post_list'), # путь для какой-то категории
     path('<slug:slug>/<slug:post_slug>/', views.PostDetailView.as_view(), name='post_single'), # путь для просмотра отдельной статьи
+
 ]
