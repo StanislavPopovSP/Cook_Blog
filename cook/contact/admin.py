4c46a2809cdec37133f6336043c774f372b65519
StanislavPopovSP <staspopov4@gmail.com>
2023-04-19 14:49:22 +0500

    Добавил модели для контактов и обратной связи, сделал миграции

--- cook/contact/admin.py
+++ cook/contact/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(ContactModel)
+class ContactModelAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'create_at', 'id']
+    list_display_links = ('name', 'email')
+
+
+admin.site.register(ContactLink)
