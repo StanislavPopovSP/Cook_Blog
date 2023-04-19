5fa942363b4d10e63a13266e19cb1e7f4a1bc4ec
StanislavPopovSP <staspopov4@gmail.com>
2023-04-19 12:37:38 +0500

    Включающий тег для вывода рецептов в верхнем меню

--- cook/blog/templatetags/menu_tags.py
+++ cook/blog/templatetags/menu_tags.py
@@ -1,12 +1,19 @@
 from django import template
-from blog.models import Category
+from blog.models import Category, Post
 
 
 register = template.Library()
 
 @register.inclusion_tag('blog/include/tags/top_menu.html')
 def get_categories():
-    """Для вывода включающего тега"""
+    """Включающий тег, вывод всех категорий и подкатегорий"""
     # category = Category.objects.filter(parent__isnull=True).order_by('name')
     category = Category.objects.all()
     return {'list_category': category}
+
+
+@register.inclusion_tag('blog/include/tags/recipes_tag.html')
+def get_last_posts():
+    """Включающий тег, забираем пять последних постов"""
+    posts = Post.objects.select_related('category').order_by('-id')[:5]
+    return {'list_last_post': posts}
