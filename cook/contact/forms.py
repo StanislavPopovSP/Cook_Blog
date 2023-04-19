738c3c3f52c7847fda5d200b00279c8fc944cbc8
StanislavPopovSP <staspopov4@gmail.com>
2023-04-19 16:00:42 +0500

    Добавил обработчик формы для отправки данных повару.

--- cook/contact/forms.py
+++ cook/contact/forms.py
@@ -3,6 +3,7 @@ from .models import ContactModel
 
 
 class ContactForm(forms.ModelForm):
+    """Создание формы, для связи с поваром"""
     class Meta:
         model = ContactModel
         fields = '__all__'
--- cook/contact/views.py
+++ cook/contact/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from .forms import ContactForm
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+
+
+class CreateContact(CreateView):
+    """Обработка формы для связи с поваром"""
+    form_class = ContactForm
+    template_name = 'blog/addpage.html'
+    success_url = reverse_lazy('home')
