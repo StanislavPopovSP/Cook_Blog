1fa6f43777f72e831fb776c466eea2d579b513ae
StanislavPopovSP <staspopov4@gmail.com>
2023-04-21 14:38:45 +0500

    Добавил путь ведущий на страницу о нас

--- cook/contact/urls.py
+++ cook/contact/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('contact/', views.ContactView.as_view(), name='contact'),
-    path('feedback/', views.CreateContact.as_view(), name='feedback'),
+    path('contact/', views.ContactView.as_view(), name='contact'), # страница контакт
+    path('feedback/', views.CreateContact.as_view(), name='feedback'), # для формы контакт
+
+    path('about/', views.AboutView.as_view(), name='about') # страница о нас
 ]
