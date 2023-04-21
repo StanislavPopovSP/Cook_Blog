d8f2113ad068c93527a17703bbf49eea7e8312cb
StanislavPopovSP <staspopov4@gmail.com>
2023-04-21 14:38:07 +0500

    Добавил дополнительные настроки вывода и регистрации моделей страницы о нас

--- cook/contact/admin.py
+++ cook/contact/admin.py
@@ -8,4 +8,17 @@ class ContactModelAdmin(admin.ModelAdmin):
     list_display_links = ('name', 'email')
 
 
+class ImageAboutInline(admin.StackedInline):
+    """Для соединения фотографий со страницей о нас"""
+    model = ImageAbout
+    extra = 1
+
+@admin.register(About)
+class AboutAdmin(admin.ModelAdmin):
+    """Регистрация и вывод нужных полей"""
+    list_display = ['title', 'id']
+    inlines = [ImageAboutInline]
+
+
 admin.site.register(ContactLink)
+admin.site.register(Social)
