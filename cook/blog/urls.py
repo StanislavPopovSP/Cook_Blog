65019b34dbff4616be173d84b8db865094c6f77e
StanislavPopovSP <staspopov4@gmail.com>
2023-04-23 20:05:34 +0500

    Добавил путь для связи комментария с постом

--- cook/blog/urls.py
+++ cook/blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'), # путь ведущий на главную страницу
 
+    path('comment/<int:pk>/', views.CreateComment.as_view(), name='create_comment'), # связь комментария с постом
     path('<slug:slug>/', views.PostListView.as_view(), name='post_list'), # путь для какой-то категории
     path('<slug:slug>/<slug:post_slug>/', views.PostDetailView.as_view(), name='post_single'), # путь для просмотра отдельной статьи
-
 ]
